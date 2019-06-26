1ea4da75952ab4e6f956546b19b3c58b9b463bdf
Georgie <gina.yo@hotmail.com>
2019-06-26 01:58:18 -0400

    1000 Dreams or More Page
    Added Isotope feature to img src divs for 1000 Dreams or More.
    Added Header for 1000 Dreams or More page.

--- 1000DreamsOrMore.php
+++ 1000DreamsOrMore.php
@@ -7,8 +7,33 @@
     </head>
 
     <body>
+      <div class="container">
+        <div class="row">
 
 
+        <div class="col-6 description">
+        <h1 class="dreamsHeadText"> 
+  1000 Dreams Or More
+</h1>
+
+        </div>
+
+        <div class="col-6 my-5" style="position: relative; left:85%">
+
+    <div id ="layout" data-isotope>
+    <img src = "" class ="box wide">
+    <img src = "" class ="box right">
+
+        <img src = "" class ="box ">
+    <img src = "" class ="box wide">
+   
+        
+
+</div>
+</div>
+
+</div>
+</div>
         <? include "1000DreamsBackground.php"?>
        
 </body>
