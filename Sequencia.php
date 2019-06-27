a474c9c14d40b3cd6b27ef17b2ae6c0b32db60b5
Georgie <gina.yo@hotmail.com>
2019-06-26 21:39:09 -0400

    Finished Sequencia Page
    Added description and video divs for Sequencia! :D

--- Sequencia.php
+++ Sequencia.php
@@ -1,13 +1,43 @@
 
   <head>
   <?include "head.php"?>
-
   <link rel="stylesheet" href="Sequencia.css" >
       <title>Georgina Yeboah: Three Worlds</title>
   
     </head>
   <body>
-  
-  
+  <div class="container">
+    <div class="row">
+
+
+          <div class="col-6 description">
+            <h1 class="seqHead"> Step into Sequencia! </h1>
+          <p class="seqText"> 
+
+            Sequencia (2014) is an interactive installation that requires the user's very presence
+            to exist. By itslef, Sequencia consists of three blank white canvases aligned horizontally
+            to one another. The first two frames have a couple of white origami figures of butterflies
+            and birds. Once the user steps into the interactive space, the canvases quickly begin to 
+            fill with colours from one canvas to the other in sequence through a projection. 
+            Users can take what they want from Sequencia but once they step out of the space the
+            canvases fade back to their blank spaces, only to be revived once again by its next 
+            visitor.  
+
+          </p>
+          </div>
+<div class="col-6 videoBox">
+
+<iframe src="https://player.vimeo.com/video/93392877" 
+  width="640" height="360" frameborder="0" style="radius-border:25px" allow="autoplay; fullscreen" allowfullscreen></iframe>
+</div>
+<div class="col-8 description" style="left:55%; top:70%; padding:10px">
+<p class="seqText"> 
+  Click play on the vimeo video and watch Sequencia unfold!
+</p>
+</div>
+
+    </div>
+      </div>
+
   </body>
   
